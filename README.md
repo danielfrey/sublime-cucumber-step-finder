700ffc58ca42a48fc113dea5f94b0ee80c28b335
Daniel Frey <df@garaio.com>
2012-12-03 21:21:50 +0100

    MatchStepCommand implemented

--- README.md
+++ README.md
@@ -2,8 +2,12 @@
 
 This plugin supports Rails developers to easily navigate to their cucumber (http://cukes.info) steps.
 
-It provides a command, listing all steps in the open project and lets you choose
+It provides by now two commands
+
+* One listing all steps in the open project and letting you choose
 a step using the built-in mechanism for search.
+* The second one letting you jump to the corresponding step by calling the "MatchStep"-Command when standing
+on a step in the features file
 
 #Installation
 ## Mac OSX (manual)
@@ -13,24 +17,37 @@ a step using the built-in mechanism for search.
 ## Linux/Windows
 Not tested yet. Contributions are welcome. If keyboard settings are provided, it should work.
 
+## Package Control
+Installation through [package control](http://wbond.net/sublime_packages/package_control) is recommended. It will handle updating your packages as they become available. To install, do the following.
 
-
-## Package manager
-As soon as it's listed in the Package Manager Channel I will update this section...
+* In the Command Palette, enter `Package Control: Install Package`
+* Search for `CucumberStepFinder`
 
 ## Usage
-The default key-binding is `cmd+y`
-Change it if it's already used in your configuration
+The default key-binding for "search" is `super + y`, respectively
+`ctrl + super + m` for "match".
+Change it if one is already used in your configuration
 
 ## Configuration
-Two settings are available so far.
+The following settings are available so far.
 
     {
        "cucumber_features_path"  :  "features",
-       "cucumber_step_pattern"   :  ".*_steps.*\\.rb"
+       "cucumber_step_pattern"   :  ".*_steps.*\\.rb",
+       "cucumber_code_keywords"  :  ["given", "when", "then", "and","but", "und", "dann", "wenn", "gegeben sei", "angenommen"]
     }
 
-The plugin looks for `cucumber_features_path` as a direct subdirectory of your project as it's the default in Rails projects. Override this setting of your steps are located in a different subdirectory.
+The plugin looks for `cucumber_features_path` as a direct subdirectory of your project as it's the default in Rails projects. Override this setting if your steps are located in a different subdirectory.
 
-In the given features directory it uses the secod setting for finding step-files recursively matching the `cucumber_step_pattern`
+In the given features directory it uses the second setting for finding step-files recursively matching the `cucumber_step_pattern`
 
+For finding the matching step, CucumberStepFinder needs to know which are the cucumber-keywords. Since there are different keywords beside English, you can configure them in `cucumber_code_keywords`.
+By default English and German are predefined.
+
+To find out, which keywords to configure e.g. German (language code `de),
+type
+
+    cucumber --18n de
+
+ into the console. The last argument is the language code.
+ Add all words marked with "code" to the settings.
--- cucumber_step_finder.py
+++ cucumber_step_finder.py
@@ -4,20 +4,15 @@ import os
 import re
 import codecs
 
-class CucumberStepFinderCommand(sublime_plugin.WindowCommand):
+class CucumberBaseCommand(sublime_plugin.WindowCommand, object):
   def __init__(self, window):
     sublime_plugin.WindowCommand.__init__(self, window)
-    self.recent_files = []
-    self.enabled      = True
     self.load_settings()
 
   def load_settings(self):
     self.settings = sublime.load_settings("CucumberStepFinder.sublime-settings")
     self.features_path = self.settings.get('cucumber_features_path')  # Default is "features"
     self.step_pattern = self.settings.get('cucumber_step_pattern')    # Default is '.*_steps.*\.rb'
-
-  def run(self, file_name=None):
-    self.find_all_steps()
 
   def find_all_steps(self):
     pattern = re.compile(r'((.*)(\/\^.*))\$\/')
@@ -40,22 +35,13 @@ class CucumberStepFinderCommand(sublime_plugin.WindowCommand):
                     if match:
                       self.steps.append((match.group(), index, step_file_path))
                     index += 1
-                      # print match.group()
-
-    steps_only = [x[0] for x in self.steps]
-    self.window.show_quick_panel(steps_only, self.step_found)
-
 
   def step_found(self, index):
     if index >= 0:
-      # print self.steps[index][2], self.steps[index][1]
       file_path = self.steps[index][2]
       view = self.window.open_file(file_path)
-
       self.active_ref = (view, self.steps[index][1])
-      # self.highlight_step(view, self.steps[index][1])
       self.mark_step()
-
 
   def mark_step(self):
 
@@ -64,8 +50,51 @@ class CucumberStepFinderCommand(sublime_plugin.WindowCommand):
     if view.is_loading():
       sublime.set_timeout(self.mark_step, 50)
     else:
-      # if view == self.active_ref[0]:
       view.run_command("goto_line", {"line": self.active_ref[1]+1} )
 
+class MatchStepCommand(CucumberBaseCommand):
+  def __init__(self, window):
+    CucumberBaseCommand.__init__(self, window)
+    self.words = self.settings.get('cucumber_code_keywords')
 
+  def run(self, file_name=None):
+    self.get_line()
 
+  def get_line(self):
+    view = self.window.active_view()
+    line_sel = view.line(view.sel()[0])
+    text_line = view.substr(line_sel).strip()
+    self.cut_words(text_line)
+
+  def cut_words(self, text):
+     upcased = [up.capitalize() for up in self.words]
+     expression = "^{0}".format('|^'.join(upcased))
+
+     pattern = re.compile(expression)
+     short_text = re.sub(pattern, '', text).strip()
+     self.find_all_steps()
+
+     step_filter = re.compile(r'.*\/\^(.*)\$\/') # map all steps
+     steps_regex = [re.match(step_filter, x[0]).group(1) for x in self.steps]
+
+     for step in self.steps:
+       step_pattern = re.match(step_filter, step[0]).group(1)
+       step_regex = re.compile(step_pattern)
+       match =  re.match(step_regex, short_text)
+       if match:
+         index =  self.steps.index(step)
+         self.step_found(index)
+       else:
+        sublime.status_message('Can\'t find a match')
+
+class CucumberStepFinderCommand(CucumberBaseCommand):
+  def __init__(self, window):
+    CucumberBaseCommand.__init__(self, window)
+
+  def run(self, file_name=None):
+    self.list_steps()
+
+  def list_steps(self):
+    self.find_all_steps()
+    steps_only = [x[0] for x in self.steps]
+    self.window.show_quick_panel(steps_only, self.step_found)
