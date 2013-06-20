e8e88adfb2769afe6ffd9bb7de1b99ef64f30791
Tom Anderson <tom@squeat.com>
2013-06-20 09:30:36 -0500

    Added '.' to list of folders that can match cucumber_features_path

--- cucumber_step_finder.py
+++ cucumber_step_finder.py
@@ -19,9 +19,9 @@ class CucumberBaseCommand(sublime_plugin.WindowCommand, object):
     self.steps = []
     folders = self.window.folders()
     for folder in folders:
-      for path in os.listdir(folder):
+      for path in os.listdir(folder) + ['.']:
         full_path = os.path.join(folder, path)
-        if path == self.features_path or self.features_path == '.':
+        if path == self.features_path:
           self.step_files = []
           for root, dirs, files in os.walk(full_path):
             for f_name in files:
